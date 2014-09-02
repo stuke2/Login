d77258b8e703645f6027c598c79f3c25f247b181
Jan Peca <pecajan@gmail.com>
2014-09-02 12:25:22 +0200

    Build script updated

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -35,8 +35,8 @@ set_time_limit(0);
 /* define version */
 define('PKG_NAME','Login');
 define('PKG_NAME_LOWER','login');
-define('PKG_VERSION','1.8.1');
-define('PKG_RELEASE','pl');
+define('PKG_VERSION','1.9.0');
+define('PKG_RELEASE','dev');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
@@ -64,6 +64,7 @@ $modx->setLogTarget('ECHO');
 
 $modx->loadClass('transport.modPackageBuilder','',false, true);
 $builder = new modPackageBuilder($modx);
+$builder->directory = dirname(dirname(__FILE__)).'/_packages/';
 $builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
 $builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
 
