b20944f444811d913dfb14441a61573fecff1e4d
Mathias Dannevang <mathias@dannevang.org>
2016-12-22 15:26:36 +0100

    Add danish translations

---
+++ core/components/login/lexicon/da/forgotpassword.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Login
+ *
+ * Copyright 2010 by Jason Coward <jason@modxcms.com> and Shaun McCormick
+ * <shaun@modxcms.com>
+ *
+ * Login is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Login is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Login; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package login
+ */
+/**
+ * Forgot Password Danish lexicon for Login
+ *
+ * @package login
+ * @subpackage lexicon
+ */
+$_lang['login.email'] = 'E-mail';
+$_lang['login.forgot_password'] = 'Glemt adganskode';
+$_lang['login.forgot_password_email_subject'] = 'Glemt adganskode email';
+$_lang['login.or_forgot_username'] = 'Eller, glemt dit brugernavn?';
+$_lang['login.reset_password'] = 'Nulstil adgangskode';
+$_lang['login.username'] = 'Brugernavn';
+$_lang['login.user_err_nf_email'] = 'Der blev ikke fundet en bruger med den e-mail.';
+$_lang['login.user_err_nf_username'] = 'Der blev ikke fundet en bruger med det brugernavn.';
