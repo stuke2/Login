e7a34c945dabdfb22c798ef5f6240febb059f863
gadgetto <martin.gartner@bitego.com>
2017-04-12 11:17:35 +0200

    Only accept login via email if it exists only once

--- core/components/login/processors/customlogin.class.php
+++ core/components/login/processors/customlogin.class.php
@@ -35,15 +35,22 @@ class CustomLoginProcessor extends modSecurityLoginProcessor {
      * @return bool|null|string
      */
     public function getUser() {
-        /** @var $user modUser */
-        $this->user = $this->modx->getObjectGraph('modUser', '{"Profile":{},"UserSettings":{}}', array(
-            array(
-                'modUser.username' => $this->username
-            ),
-            array(
-                'OR:Profile.email:=' => $this->username
-            )
+
+        // Only accept login via email address if it exists only once!
+        $count = $this->modx->getCount('modUserProfile', array(
+            'email' => $this->username,
         ));
+        if ($count > 1) {
+            $criteria = array ('modUser.username' => $this->username);
+        } else {
+            $criteria = array(
+                array('modUser.username' => $this->username),
+                array('OR:Profile.email:=' => $this->username)
+            );
+        }
+
+        /** @var $user modUser */
+        $this->user = $this->modx->getObjectGraph('modUser', '{"Profile":{},"UserSettings":{}}', $criteria);
         return $this->fireOnUserNotFoundEvent();
     }
 }
